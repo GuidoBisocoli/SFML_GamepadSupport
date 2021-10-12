4bd5614761a4c8500a9a8cade1ccc697ac961e98
Guido Dev <34041274+GuidoBisocoli@users.noreply.github.com>
2021-10-12 17:38:58 -0300

    Create Gamepad.h

---
+++ Gamepad.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "Box2D\Box2D.h"
+#include "SFML\Window.hpp"
+
+class Gamepad
+{
+	private:
+		struct GamePadAxis {
+			sf::Joystick::Axis Axis;
+			bool inverted; // inverted: from 0 to -100
+		};
+		struct Trigger {
+			bool isAxis;
+			GamePadAxis Axis;
+			int button;
+		};
+	public:
+		Gamepad(int number, sf::Joystick::Identification data);
+		~Gamepad() {}
+
+		enum GAMEPAD_BUTTON { btn_a, btn_b, btn_x, btn_y, btn_leftStick, btn_rightStick, btn_back, btn_start, btn_lb, btn_rb, dpad_up, dpad_down , dpad_left, dpad_right };
+		enum GAMEPAD_AXIS { leftStick_X, leftStick_Y, rightStick_X, rightStick_Y };
+		enum GAMEPAD_TRIGGER { leftTrigger, rightTrigger };
+		
+		bool isButtonPressed(GAMEPAD_BUTTON btn);
+		float getAxisPosition(GAMEPAD_AXIS axis);
+		float getTriggerValue(GAMEPAD_TRIGGER tgr);
+	private:
+		int gamepadNumber;
+		bool isXInput;
+
+		void LoadData(std::string line);
+		bool assignButton(std::string n, std::string d);
+		bool assignAxis(std::string n, std::string d);
+		sf::Joystick::Axis extractAxis(int axisNumber);
+		
+		int enumToButton(GAMEPAD_BUTTON btn);
+		float shrinkValue(float f, bool trigger);
+
+		GamePadAxis LSTICK_X;
+		GamePadAxis LSTICK_Y;
+		int LSTICK;
+		GamePadAxis RSTICK_X;
+		GamePadAxis RSTICK_Y;
+		int RSTICK;
+		GamePadAxis POV_X;
+		GamePadAxis POV_Y;
+		Trigger LTRIGGER;
+		Trigger RTRIGGER;
+		int LB;
+		int RB;
+		int BACK;
+		int START;
+		int A;
+		int B;
+		int X;
+		int Y;
+};
